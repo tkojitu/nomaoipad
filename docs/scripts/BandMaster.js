205cd66214df569fc4c0843fd47b0982b4192e43
tko <tko@jitu.org>
2020-02-26 23:32:54 +0900

    use map.

--- docs/scripts/BandMaster.js
+++ docs/scripts/BandMaster.js
@@ -1,6 +1,10 @@
 export default class {
 	constructor() {
-		this.frequencies = [
+		this.dictionary = this.makeDictionary();
+	}
+
+	makeDictionary() {
+		let freqs = [
 			8.1757989156,  // 0
 			8.6619572180,
 			9.1770239974,
@@ -130,51 +134,146 @@ export default class {
 			11839.8215267723,
 			12543.8539514160 // 127
 		];
+		let notes = [
+			"C-1", // 0
+			"C#-1",
+			"D-1",
+			"D#-1",
+			"E-1",
+			"F-1",
+			"F#-1",
+			"G-1",
+			"G#-1",
+			"A-1",
+			"A#-1", // 10
+			"B-1",
+			"C0",
+			"C#0",
+			"D0",
+			"D#0",
+			"E0",
+			"F0",
+			"F#0",
+			"G0",
+			"G#0", // 20
+			"A0",
+			"A#0",
+			"B0",
+			"C1",
+			"C#1",
+			"D1",
+			"D#1",
+			"E1",
+			"F1",
+			"F#1", // 30
+			"G1",
+			"G#1",
+			"A1",
+			"A#1",
+			"B1",
+			"C2",
+			"C#2",
+			"D2",
+			"D#2",
+			"E2", // 40
+			"F2",
+			"F#2",
+			"G2",
+			"G#2",
+			"A2",
+			"A#2",
+			"B2",
+			"C3",
+			"C#3",
+			"D3", // 50
+			"D#3",
+			"E3",
+			"F3",
+			"F#3",
+			"G3",
+			"G#3",
+			"A3",
+			"A#3",
+			"B3",
+			"C4", // 60
+			"C#4",
+			"D4",
+			"D#4",
+			"E04",
+			"F4",
+			"F#4",
+			"G4",
+			"G#4",
+			"A4",
+			"A#4", // 70
+			"B4",
+			"C5",
+			"C#5",
+			"D5",
+			"D#5",
+			"E5",
+			"F5",
+			"F#5",
+			"G5",
+			"G#5", // 80
+			"A5",
+			"A#5",
+			"B5",
+			"C6",
+			"C#6",
+			"D6",
+			"D#6",
+			"E6",
+			"F6",
+			"F#6", // 90
+			"G6",
+			"G#6",
+			"A6",
+			"A#6",
+			"B6",
+			"C7",
+			"C#7",
+			"D7",
+			"D#7",
+			"E7", // 100
+			"F7",
+			"F#7",
+			"G7",
+			"G#7",
+			"A7",
+			"A#7",
+			"B7",
+			"C8",
+			"C#8",
+			"D8", // 110
+			"D#8",
+			"E8",
+			"F8",
+			"F#8",
+			"G8",
+			"G#8",
+			"A8",
+			"A#8",
+			"B8",
+			"C9", // 120
+			"C#9",
+			"D9",
+			"D#9",
+			"E9",
+			"F9",
+			"F#\9",
+			"G9" // 127
+		];
+		let dic = new Map();
+		for (let i = 0; i < freqs.length; ++i) {
+			dic.set(notes[i], freqs[i]);
+		}
+		return dic;
 	}
 
 	noteOn(note) {
 	}
 
 	noteOff(note) {
-	}
-
-	parseNote(noteName) {
-		if (noteName.length == 0) {
-			return -1;
-		}
-		let match = noteName.match(/^([ABCDEFG])(#?)(-?\d+)/);
-		let offset = this.getBaseNoteOffset(match[1]);
-		if (offset < 0) {
-			alert("Illegal note name: " + noteName);
-			return -1;
-		}
-		offset += (match[2] == "#") ? 1 : 0;
-		let octave = this.toMidiOctaveOffset(parseInt(match[3]));
-		return offset + octave;
-	}
-
-	getBaseNoteOffset(str) {
-		switch (str) {
-		case "C":
-			return 0;
-		case "D":
-			return 2;
-		case "E":
-			return 4;
-		case "F":
-			return 5;
-		case "G":
-			return 7;
-		case "A":
-			return 9;
-		case "B":
-			return 11;
-		default:
-			return -1;
-		}
-	}
-
-	toMidiOctaveOffset(octave) {
-		return (octave + 1) * 12;
 	}
 }
